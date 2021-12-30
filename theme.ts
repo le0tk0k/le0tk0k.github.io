777b89b0d38468164dbc7cfe0d8c494f48e574aa
Leo Takaoka <le0tk0k@gmail.com>
2021-12-30 11:46:10 +0900

    feat: add custom color

--- theme.ts
+++ theme.ts
@@ -2,6 +2,32 @@ import { extendTheme } from '@chakra-ui/react'
 
 const customTheme = extendTheme({
   config: {},
+  colors: {
+    polarNight: {
+      100: '#4C566A',
+      200: '#434C5E',
+      300: '#3B4252',
+      400: '#2E3440',
+    },
+    snowStorm: {
+      100: '#ECEFF4',
+      200: '#E5E9F0',
+      300: '#D8DEE9',
+    },
+    frost: {
+      100: '#8FBCBB',
+      200: '#88C0D0',
+      300: '#81A1C1',
+      400: '#5E81AC',
+    },
+    aurora: {
+      100: '#BF616A',
+      200: '#D08770',
+      300: '#EBCB8B',
+      400: '#A3BE8C',
+      500: '#B48EAD',
+    },
+  },
 })
 
 export default customTheme
