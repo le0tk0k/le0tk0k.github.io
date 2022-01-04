02eeb2d65910bda280f5c0426f5bc4020a82d3dd
Leo Takaoka <le0tk0k@gmail.com>
2022-01-04 14:42:08 +0900

    feat: post list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,38 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import matter from 'gray-matter'
+import path from 'path'
+import fs from 'fs'
 import Container from 'components/container'
+import Post from 'components/post'
+import { postFilePaths, POSTS_PATH } from 'utils/mdx'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
     <Container>
-      <div></div>
+      {posts.map((post) => (
+        <Post key={post.data.title} {...post} />
+      ))}
     </Container>
   )
+}
+
+export function getStaticProps() {
+  const posts = postFilePaths
+    .map((filePath) => {
+      const source = fs.readFileSync(path.join(POSTS_PATH, filePath))
+      const { data } = matter(source)
+
+      return {
+        data,
+        filePath,
+      }
+    })
+    .sort(
+      (x, y) => Number(new Date(y.data.date)) - Number(new Date(x.data.date)),
+    )
+
+  return { props: { posts } }
 }
 
 export default Home
